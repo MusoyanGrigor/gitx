70bca84fc058d0ead4a79c7fa198d15620165a0b
MusoyanGrigor <musoyangrigor251@gmail.com>
2026-04-04 23:21:51 +0400

    build: zero-warning binary update

--- src/utils/styles.rs
+++ src/utils/styles.rs
@@ -30,21 +30,8 @@ impl TreeStyle {
         }
     }
 
-    pub fn hash() -> ContentStyle {
-        ContentStyle::new().with(Color::AnsiValue(242))
-    }
-
     pub fn subject() -> ContentStyle {
         ContentStyle::new().with(Color::White).attribute(Attribute::Bold)
-    }
-
-    pub fn metadata() -> ContentStyle {
-        // Very dim gray for metadata
-        ContentStyle::new().with(Color::AnsiValue(238))
-    }
-
-    pub fn separator() -> ContentStyle {
-        ContentStyle::new().with(Color::AnsiValue(236))
     }
 
     // --- Badges ---
@@ -63,10 +50,6 @@ impl TreeStyle {
 
     pub fn tag_badge() -> ContentStyle {
         ContentStyle::new().with(Color::Yellow)
-    }
-
-    pub fn ref_divider() -> ContentStyle {
-        ContentStyle::new().with(Color::AnsiValue(236))
     }
 }
 
